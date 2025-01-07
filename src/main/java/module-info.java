9356f7b388eba2c4b445cde114cdc4e1ab31d3a2
memoglow <lehiep2004.lh@gmail.com>
2025-01-07 21:07:36 +0700

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.noface.flashcard {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.noface.flashcard to javafx.fxml;
+    exports com.noface.flashcard;
+}
